e821875de61c5e4ed9c2e81c2d56030979fa45d0
RuqayyahSara <sarasadiya4@gmail.com>
2023-04-11 10:05:21 +0000

    Adding performance metrics to the system calls amde

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-const { execFile } =  require("child_process");
-const fs = require("fs/promises");
+const { execFile } = require("child_process");
+const { performance, PerformanceObserver } = require("perf_hooks")
 const os = require("os")
 const readlineSync = require("readline-sync")
 
-const backup = async ()=>{
- try {
-console.clear()
-    console.log("------------------------------------------------------------")
-    console.log("\t CREATE BACKUPS")
-    console.log("------------------------------------------------------------")
+let metrics = {}
+const backup = async () => {
+    try {
+        console.clear()
+        console.log("------------------------------------------------------------")
+        console.log("\t CREATE BACKUPS")
+        console.log("------------------------------------------------------------")
 
 
- let folderName = readlineSync.question("Enter Absolute Path of the Folder that you want to backup: ")
- let osType = os.type();
- osType = osType.toLowerCase();
+        let folderName = readlineSync.question("Enter Absolute Path of the Folder that you want to backup: ")
+        let osType = os.type();
+        osType = osType.toLowerCase();
 
- if (osType !== "linux") return
+        if (osType !== "linux") return
 
-    let output = await bashCompiler(folderName)
-    let msg;
-    if (output.statuscode || output.statuscode == null) msg = "\nError. Enter Correct Path";
-    else msg = "\nBackup Successfully created";
-    output.msg = msg;
-    console.log(output.msg);
+        let output = await bashCompiler(folderName)
+        let msg;
+        if (output.statuscode || output.statuscode == null) msg = "\nError. Enter Correct Path";
+        else msg = "\nBackup Successfully created";
+        output.msg = msg;
+        console.log(output.msg);
     }
-  catch (err) {
-    console.log(err)
-  }
+    catch (err) {
+        console.log(err)
+    }
 }
 
-backup()
+// backup()
+
+const wrapped = performance.timerify(backup);
+const obs = new PerformanceObserver((list) => {
+    metrics.time = (list.getEntries()[0].duration / 1000).toFixed(6);
+    console.log(metrics)
+    obs.disconnect();
+});
+obs.observe({ entryTypes: ['function'] });
+wrapped()
+metrics.memory = `${Math.floor(process.memoryUsage().heapUsed / 1024)}`;
 
 function bashCompiler(folder) {
     return new Promise((resolve, reject) => {
-       let output = {};
-        const child = execFile("zip", ['-r','-q','backup', folder] , { maxBuffer: 1024 * 90, timeout: 6000 }, (err, stdout, stderr) => {
+        let output = {};
+        const child = execFile("zip", ['-r', '-q', 'backup', folder], { maxBuffer: 1024 * 90, timeout: 6000 }, (err, stdout, stderr) => {
             if (stdout) {
                 output.stdout = stdout;
                 // console.log(stdout);
